7ce1510883828fe8e121fdde28ac312ceecce8e8
mark ronald sicad <markronaldsicad@gmail.com>
2023-02-06 07:05:42 +0800

    change port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const http = require('http')
-const PORT = process.env.PORT || 8000
+const PORT = 9000
 const app = express()
 const server = http.createServer(app)
 const io = require('socket.io')(server)
